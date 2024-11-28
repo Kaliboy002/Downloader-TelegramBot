33c163718571c245e25e325b2484cecbfc4a5552
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-28 10:45:43 +0430

    Update Config.py

--- Config/Config.py
+++ Config/Config.py
@@ -1,7 +1,7 @@
 
 class Config:
    SESSION_NAME = "bot"
-   API_ID = ...
-   API_HASH = ""
-   BOT_TOKEN = ""
-   BOT_USERNAME = "" # without @ example : Testbot
+   API_ID = 15787995
+   API_HASH = "e51a3154d2e0c45e5ed70251d68382de"
+   BOT_TOKEN = "7964758764:AAGuYr8voir-lrOBUim_mj1w2KfY1-7rsgI"
+   BOT_USERNAME = "Kaliboy002" # without @ example : Testbot
